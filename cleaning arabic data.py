11b0e83c58c08ad98dc9fd9ffdc4cfcd9aec51a3
Kerolis Youssif Nasr <kerlisyoussif12@gmail.com>
2020-09-30 19:49:29 +0200

    Update cleaning arabic data.py

--- cleaning arabic data.py
+++ cleaning arabic data.py
@@ -1,26 +1,12 @@
 
-
-
 import pandas as pd
 import os 
 import re
 
-
-
-
-
 dir_path = os.path.dirname(os.path.realpath('__file__'))
 print(dir_path)
 
-
-
-
-
 data=pd.read_csv(dir_path+"/regular_ex_Ar.csv")
-
-
-
-
 
 def cleaning (data) :
     w=[]
@@ -38,28 +24,12 @@ def cleaning (data) :
         w.append(s)
     return w
 
-
-
-
 print ("data before cleaning :")
 print(data['text'])
 
-
-
-
-
 data['text']=cleaning(data['text'])
-
-
-
-
 
 print ("data after cleaning :")
 print (data['text'])
 
-
-# In[8]:
-
-
 data.to_csv(dir_path+'/data_after_cleaning.csv')
-
